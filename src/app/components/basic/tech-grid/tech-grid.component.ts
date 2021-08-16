94384baef9b76de2619c1e70d0be840288bee913
Karan Raina <karanraina1996@gmail.com>
2021-08-16 21:32:48 +0530

    finalized profile

--- src/app/components/basic/tech-grid/tech-grid.component.ts
+++ src/app/components/basic/tech-grid/tech-grid.component.ts
@@ -12,6 +12,10 @@ export class TechGridComponent implements OnInit {
       logo: 'assets/nodejs.svg',
     },
     {
+      title: 'Express Js',
+      logo: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg',
+    },
+    {
       title: 'React JS',
       logo: 'assets/reactjs.svg',
     },
@@ -23,6 +27,37 @@ export class TechGridComponent implements OnInit {
       title: 'Next Js',
       logo: 'assets/nextjs.svg',
     },
+    {
+      title: 'PostgreSQL',
+      logo: 'assets/postgres.png',
+    },
+    {
+      title: 'HTML5',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+    },
+    {
+      title: 'CSS3',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+    },
+    {
+      title: 'Heroku',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Heroku_logo.svg',
+      width: '60px',
+    },
+    {
+      title: 'MS Azure',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a8/Microsoft_Azure_Logo.svg',
+      width: '60px',
+    },
+    {
+      title: 'OpenShift',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/OpenShift-LogoType.svg',
+    },
+    {
+      title: 'Webpack',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/9/94/Webpack.svg',
+      width: '60px'
+    }
   ];
   constructor() { }
 
