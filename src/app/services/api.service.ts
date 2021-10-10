2063e204ba9744be65aa39af3f1504af0969c1e0
Karan Raina <karanraina1996@gmail.com>
2021-10-10 09:04:32 +0530

    feature: add cache

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs'
+import { Observable, of, throwError } from 'rxjs'
 import { map, filter } from 'rxjs/operators';
 import { Blog } from '../models/blog';
+import { LocalCacheService } from './local-cache.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,42 @@ export class ApiService {
   host: string = 'https://karankraina-api.vercel.app/api/';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private cache: LocalCacheService
   ) { }
 
-  getUrlEndpoint(endpoint: string): string {
+  private getUrlEndpoint(endpoint: string): string {
     return `${this.host}${endpoint}`;
   }
 
+  private httpGet(endpoint: string): Observable<any> {
+    const dataFromCache = this.cache.getCache(endpoint);
+    if (dataFromCache) {
+      return  of(dataFromCache);
+    }
+    const fullUrl = this.getUrlEndpoint(endpoint);
+    return this.http.get(fullUrl).pipe(
+      map(
+        (response: Blog[]) => {
+          this.cache.setCache(endpoint, response);
+          return response;
+        }
+      )
+    )
+
+  }
+
   getBlogs(): Observable<any> {
+    return this.httpGet('blogs');
     return this.http.get(this.getUrlEndpoint('blogs'));
   }
 
   getBlogById(id: string): Observable<any> {
+    return this.httpGet('blogs').pipe(
+      map(
+        (response: Blog[]) => response.find(blog => blog.id === id)
+      )
+    );
     return this.http.get(this.getUrlEndpoint('blogs')).pipe(
       map(
         (response: Blog[]) => response.find(blog => blog.id === id)
@@ -32,6 +57,7 @@ export class ApiService {
   }
 
   getInstagramPosts() {
+    return this.httpGet('instagram');
     return this.http.get(this.getUrlEndpoint('instagram'));
   }
 }
